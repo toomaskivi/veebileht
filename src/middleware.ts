4eb1d56c731844c82a0ab456b3f031b486b14c46
Markus Tarn <markustarn@gmail.com>
2025-01-21 16:29:58 +0200

    Parandasin inglise keele middlewarei ja uuendasin readme faili

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
-import { languages } from './languages';
 
 export default createMiddleware(routing);
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', `/(${languages.join('|')})/:path*`]
+  matcher: ['/', '/(et|en)/:path*'],
 };
